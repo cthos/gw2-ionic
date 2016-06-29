ee775d834bf6f5772bd460eabbc50a12ca4de8f0
cthos <daginus@gmail.com>
2016-06-28 17:21:58 -0700

    Updating the GW2API service to be of the proper format.

--- www/js/services.js
+++ www/js/services.js
@@ -5,71 +5,77 @@ angular.module('app.services', []);
 
   angular
     .module('app.services')
-    .factory('GW2API', function () {
-      var Achievements = {
-        'pve': [],
-        'pvp': [],
-        'wvw': []
-      };
-      var gw2 = require('gw2-api');
-      var api = new gw2.gw2();
-      //api.setStorage(new gw2.memStore())
-      //api.setCache(false);
-      api.setUseAuthHeader(false);
+    .factory('GW2API', GW2API);
 
-      return {
-        achivements: Achievements,
+  GW2API.$inject = [];
+  function GW2API() {
+    var Achievements = {
+      'pve': [],
+      'pvp': [],
+      'wvw': []
+    };
+    var gw2 = require('gw2-api');
+    var api = new gw2.gw2();
+    //api.setStorage(new gw2.memStore())
+    //api.setCache(false);
+    api.setUseAuthHeader(false);
 
-        // For those times you want to interact directly with the API.
-        api: api,
+    var service = {
+      achievements: Achievements,
+      api: api,
+      reload: reload,
+      tokenHasPermission: tokenHasPermission
+    };
 
-        reload: function () {
-          var achIds = {};
-          return api.getDailyAchievements().then(function (achs) {
-            var lookups = [];
-            for (var type in achs) {
-              if (typeof Achievements[type] === 'undefined') {
-                continue;
-              }
-              achIds[type] = [];
-              achs[type].forEach(function (a) {
-                achIds[type].push(a.id);
-                lookups.push(a.id);
-              });
-            }
-            return api.getAchievements(lookups);
-          }).then(function (achs) {
-            Achievements = {
-              'pve': [],
-              'pvp': [],
-              'wvw': []
-            };
+    return service;
 
-            achs.forEach(function (a) {
-              for (var type in achIds) {
-                if (achIds[type].indexOf(a.id) > -1) {
-
-                  Achievements[type].push(a);
-                  continue;
-                }
-              }
-            });
-            return Achievements;
-          }).catch(function (e) {
-            console.error(e);
-          });
-        },
-
-        tokenHasPermission: function (permission) {
-          if (!this.tokenPerms) {
-            var that = this;
-            return api.getTokenInfo().then(function (info) {
-              that.tokenPerms = info.permissions;
-            });
+    ////////////////
+    function reload() {
+      var achIds = {};
+      return api.getDailyAchievements().then(function (achs) {
+        var lookups = [];
+        for (var type in achs) {
+          if (typeof Achievements[type] === 'undefined') {
+            continue;
           }
+          achIds[type] = [];
+          achs[type].forEach(function (a) {
+            achIds[type].push(a.id);
+            lookups.push(a.id);
+          });
         }
-      };
-    })
+        return api.getAchievements(lookups);
+      }).then(function (achs) {
+        Achievements = {
+          'pve': [],
+          'pvp': [],
+          'wvw': []
+        };
+
+        achs.forEach(function (a) {
+          for (var type in achIds) {
+            if (achIds[type].indexOf(a.id) > -1) {
+
+              Achievements[type].push(a);
+              continue;
+            }
+          }
+        });
+        return Achievements;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
+
+    function tokenHasPermission() {
+      if (!this.tokenPerms) {
+        var that = this;
+        return api.getTokenInfo().then(function (info) {
+          that.tokenPerms = info.permissions;
+        });
+      }
+    }
+  }
 })();
 (function () {
     'use strict';
