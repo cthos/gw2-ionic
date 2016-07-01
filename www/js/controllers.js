88e827adb2f608009226a7a770ebd4df72d20bac
cthos <daginus@gmail.com>
2016-06-30 17:28:58 -0700

    Popups on trait clicky. . for rendering.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,47 +19,64 @@ angular.module('app.controllers', ['ionic']);
     }
   }
 })();
-(function () {
+(function ()
+{
   'use strict';
 
   angular
     .module('app.controllers')
     .controller('CharacterBuildCtrl', CharacterBuildCtrl);
 
-  CharacterBuildCtrl.$inject = ['GW2API', '$ionicLoading', '$stateParams'];
-  function CharacterBuildCtrl(GW2API, $ionicLoading, $stateParams) {
+  CharacterBuildCtrl.$inject = ['GW2API', '$scope', '$ionicLoading', '$stateParams', '$ionicPopup'];
+  function CharacterBuildCtrl(GW2API, $scope, $ionicLoading, $stateParams, $ionicPopup)
+  {
     var vm = this;
-
+    vm.showTraitDetails = showTraitDetails;
 
     activate();
 
     ////////////////
 
-    function activate() {
+    function activate()
+    {
       $ionicLoading.show({
         template: 'Loading...'
       });
 
-      GW2API.api.getCharacters($stateParams.charname).then(function (character) {
+      GW2API.api.getCharacters($stateParams.charname).then(function (character)
+      {
         vm.character = character;
         console.log(character.specializations);
         $ionicLoading.hide();
       })
-        .then(function () {
+        .then(function ()
+        {
           loadSpecializations();
           loadTraits();
         })
-        .catch(function (e) {
+        .catch(function (e)
+        {
           console.log(e);
         });
     }
 
-    function loadSpecializations() {
-      return GW2API.api.getProfessionSpecializations(vm.character.profession).then(function (specializations) {
+    function showTraitDetails(trait)
+    {
+      var myPopup = $ionicPopup.alert({
+        template: trait.description,
+        title: trait.name
+      });
+    }
+
+    function loadSpecializations()
+    {
+      return GW2API.api.getProfessionSpecializations(vm.character.profession).then(function (specializations)
+      {
         console.log(specializations);
         for (var env in vm.character.specializations) {
           for (var i = 0; i < vm.character.specializations[env].length; i++) {
-            specializations.forEach(function (spec) {
+            specializations.forEach(function (spec)
+            {
               if (!vm.character.specializations[env][i]) {
                 return;
               }
@@ -73,42 +90,52 @@ angular.module('app.controllers', ['ionic']);
         }
         console.log("Specs normalized");
         console.log(vm.character.specializations);
-      }).catch(function (e) {
+      }).catch(function (e)
+      {
         console.log(e);
       });
     }
 
-    function loadTraits() {
+    function loadTraits()
+    {
       var traitIds = [];
       for (var env in vm.character.specializations) {
         for (var i = 0; i < vm.character.specializations[env].length; i++) {
-           if (!vm.character.specializations[env][i]) {
+          if (!vm.character.specializations[env][i]) {
             continue;
           }
           traitIds = traitIds.concat(vm.character.specializations[env][i].traits);
         }
       }
 
-      return GW2API.api.getTraits(traitIds).then(function (traits) {
-        for (var env in vm.character.specializations) {
-          for (var i = 0; i < vm.character.specializations[env].length; i++) {
-            if (!vm.character.specializations[env][i]) {
-              continue;
-            }
-            for (var j = 0; j < vm.character.specializations[env][i].traits.length; j++) {
-              traits.forEach(function (trait) {
-                if (!vm.character.specializations[env][i].traits[j]) {
-                  return;
-                }
-                if (trait.id != vm.character.specializations[env][i].traits[j]) {
-                  return;
-                }
-                vm.character.specializations[env][i].traits[j] = trait;
-              });
+      return GW2API.api.getTraits(traitIds).then(function (traits)
+      {
+        $scope.$evalAsync(function ()
+        {
+          for (var env in vm.character.specializations) {
+            for (var i = 0; i < vm.character.specializations[env].length; i++) {
+              if (!vm.character.specializations[env][i]) {
+                continue;
+              }
+              for (var j = 0; j < vm.character.specializations[env][i].traits.length; j++) {
+                traits.forEach(function (trait)
+                {
+                  if (!vm.character.specializations[env][i].traits[j]) {
+                    return;
+                  }
+                  if (trait.id != vm.character.specializations[env][i].traits[j]) {
+                    return;
+                  }
+
+
+                  vm.character.specializations[env][i].traits[j] = trait;
+                });
+              }
             }
           }
-        }
-      }).catch(function (e) {
+        });
+      }).catch(function (e)
+      {
         console.log(e);
       });
     }
