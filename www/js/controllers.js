5b433e9fd0495731c532cb3e16fab38f709a5d36
cthos <daginus@gmail.com>
2016-07-12 20:37:52 -0700

    Render the recipes known. I think a search interface is going to be necessary

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -427,7 +427,8 @@ angular.module('app.controllers', ['ionic']);
   CharacterRecipiesCtrl.$inject = ['GW2API', '$ionicLoading', '$stateParams', '$scope'];
   function CharacterRecipiesCtrl(GW2API, $ionicLoading, $stateParams, $scope) {
     var vm = this;
-    
+    vm.outputItems = {};
+    vm.inputItems = {};
 
     activate();
 
@@ -451,17 +452,33 @@ angular.module('app.controllers', ['ionic']);
     
     function loadRecipes() {
       var outputItems = [];
+
       GW2API.api.getDeeperInfo(GW2API.api.getRecipes, vm.character.recipes).then(function (r) {
           r.forEach(function (rec) {
             outputItems.push(rec.output_item_id);
           });
 
-          console.log(outputItems);
+          vm.recipes = r;
+          loadOutputItems(outputItems);
+      }).catch(function (e) {
+        console.log(e);
       });
     }
 
-    function loadOutputItems() {
-      
+    function loadOutputItems(outputItems) {
+      GW2API.api.getDeeperInfo(GW2API.api.getItems, outputItems).then(function (items) {
+        items.forEach(function (i) {
+          vm.outputItems[i.id] = i;
+        });
+        $ionicLoading.hide();
+        $scope.$evalAsync(function () {});
+      }).catch(function (e) {
+        console.log(e);
+      });
+    }
+
+    function loadInputItems() {
+
     }
   }
 })();
