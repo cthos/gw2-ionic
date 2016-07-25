bb475798e920b9abf7c80bee79f56d4b54aa0b08
cthos <daginus@gmail.com>
2016-07-24 18:15:50 -0700

    item translation on sells, and adding the info to the app.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -709,28 +709,65 @@ angular.module('app.controllers', ['ionic']);
     function activate() { }
   }
 })();
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
   angular
     .module('app.controllers')
     .controller('TransactionsCurrentCtrl', TransactionsCurrentCtrl);
 
-  TransactionsCurrentCtrl.$inject = ['GW2API', '$ionicLoading'];
-  function TransactionsCurrentCtrl(GW2API) {
+  TransactionsCurrentCtrl.$inject = ['GW2API', '$ionicLoading', '$scope'];
+  function TransactionsCurrentCtrl(GW2API, $ionicLoading, $scope) {
+    var moment = require('moment');
     var vm = this;
+    vm.buys = [];
+    vm.sells = [];
 
     activate();
 
     ////////////////
 
-    function activate() { 
-      GW2API.api.getCommerceTransactions(true, 'buys').then(function (buys) {
-        console.log(buys);
+    function activate() {
+      GW2API.api.getCommerceTransactions(true, 'buys')
+        .then(addItemsToResults)
+        .then(function (buys) {
+          $scope.$evalAsync(function () {
+            vm.buys = buys;
+          });
+        });
+
+      GW2API.api.getCommerceTransactions(false, 'sells')
+        .then(addItemsToResults)
+        .then(function (sells) {
+          $scope.$evalAsync(function () {
+            console.log(sells);
+            vm.sells = sells;
+          });
+        });
+    }
+
+    // TODO: Genericmake
+    function addItemsToResults(res) {
+      var itemIds = [];
+
+      res.forEach(function(result) {
+        itemIds.push(result.item_id);
       });
 
-      GW2API.api.getCommerceTransactions(false, 'sells').then(function (sells) {
-        console.log(sells);
+      return GW2API.api.getItems(itemIds).then(function (items) {
+        items.forEach(function(item) {
+          item.item_id = item.id;
+          delete item.id;
+
+          for (var i = 0; i < res.length; i++) {
+            if (res[i].item_id == item.item_id) {
+              Object.assign(res[i], item);
+              res[i].formatted_date = moment(res[i].purchased).format('MMMM Do YYYY, h:mm:ss a');
+            }
+          }
+        });
+
+        return res;
       });
     }
   }
