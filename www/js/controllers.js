a633142667488ae58007f009131dc36a36fa3991
cthos <daginus@gmail.com>
2016-08-14 19:02:09 -0700

    Adding pull to refresh to character detail page.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -276,8 +276,8 @@ angular.module('app.controllers', ['ionic']);
     }
   }
 })();
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
   angular
     .module('app.controllers')
@@ -286,17 +286,33 @@ angular.module('app.controllers', ['ionic']);
   CharacterViewCtrl.$inject = ['$scope', '$ionicLoading', '$stateParams', 'GW2API'];
   function CharacterViewCtrl($scope, $ionicLoading, $stateParams, GW2API) {
     var vm = this;
-    
+
+    vm.reload = reload;
+
     activate();
 
     ////////////////
 
-    function activate() { 
+    function activate() {
       $ionicLoading.show();
 
-      GW2API.api.getCharacters($stateParams.charname).then(function (character) {
-        vm.character = character;
+      loadCharacters().then(function () {
         $ionicLoading.hide();
+      });
+    }
+
+    function loadCharacters() {
+      return GW2API.api.getCharacters($stateParams.charname).then(function (character) {
+        vm.character = character;
+      });
+    }
+
+    function reload() {
+      GW2API.api.setCache(false);
+
+      loadCharacters().then(function () {
+        GW2API.api.setCache(true);
+        $scope.$broadcast('scroll.refreshComplete');
       });
     }
   }
