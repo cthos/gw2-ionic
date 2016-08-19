132a17b02c1b3345b774b2564ded6edb72e94a85
cthos <daginus@gmail.com>
2016-08-18 20:16:18 -0700

    More testing.

--- tests/services/gw2api.js
+++ tests/services/gw2api.js
@@ -1,15 +1,29 @@
 describe('GW2API', function () {
-  var GW2API, $scope;
+  var GW2API, $scope, $q;
   var expect = chai.expect;
 
-
-  beforeEach(module('app'));
   beforeEach(module('app.services'));
   
-  beforeEach(inject(function (_GW2API_, $rootScope) {
+  beforeEach(inject(function (_GW2API_, _$q_, $rootScope) {
    GW2API = _GW2API_;
    $scope = $rootScope;
+   $q = _$q_;
   }));
+
+  beforeEach(function () {
+    sinon.stub(GW2API, 'reload', function () {
+      return $q(function (fulfill, reject) {
+        GW2API.achievements = {
+          pve : []
+        };
+        return fulfill();
+      });
+    });
+  });
+
+  afterEach(function () {
+    GW2API.reload.restore();
+  });
 
   it('Should have a GW2API Service', function () {
     expect(GW2API).to.exist;
@@ -22,6 +36,13 @@ describe('GW2API', function () {
   });
 
   it('Should have cache enabled', function () {
-    expect(GW2API.api.getCache()).to.be.false;
+    expect(GW2API.api.getCache()).to.be.true;
+  });
+
+  it('Should load daily pve', function () {
+    GW2API.reload();
+    $scope.$digest();
+
+    expect(GW2API.achievements.pve).to.exist;
   });
 });
