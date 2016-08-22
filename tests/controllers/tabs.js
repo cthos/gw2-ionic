1bc1e7fd734fb2f887cf06c015d1a926e1c59841
cthos <daginus@gmail.com>
2016-08-21 17:32:15 -0700

    Adds Tab Controller More existence tests.

--- tests/controllers/tabs.js
+++ tests/controllers/tabs.js
@@ -1,5 +1,5 @@
 describe('Tabs SubControllers', function () {
-  var PVECtrl, $scope;
+  var PVECtrl, PVPCtrl, WVWCtrl, $scope;
   var alertSpy;
   var expect = chai.expect;
 
@@ -9,10 +9,20 @@ describe('Tabs SubControllers', function () {
 
   beforeEach(inject(function ($rootScope, $controller) {
    PVECtrl = $controller('PVECtrl', {$scope: $rootScope.$new()});
+   PVPCtrl = $controller('PVPCtrl', {$scope: $rootScope.$new()});
+   WVWCtrl = $controller('WVWCtrl', {$scope: $rootScope.$new()});
    $scope = $rootScope;
   }));
 
   it('Should have a PVECtrl', function () {
     expect(PVECtrl).to.exist;
+  });
+
+  it('Should have a PVPCtrl', function () {
+    expect(PVPCtrl).to.exist;
+  });
+
+  it('Should have a WVWCtrl', function () {
+    expect(WVWCtrl).to.exist;
   });
 });
